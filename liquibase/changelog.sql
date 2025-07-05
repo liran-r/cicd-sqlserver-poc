fde6a5bc19ca9a2110636ffc37dfa9f3d080b2fb
liran-r <1234liran.reznik@gmail.com>
2025-07-05 17:11:06 -0400

    Add orders table (changeset 3)

--- liquibase/changelog.sql
+++ liquibase/changelog.sql
@@ -13,3 +13,14 @@ CREATE TABLE users (
 --changeset you:2
 ALTER TABLE users
   ADD signup_date DATETIME DEFAULT(GETDATE()) NOT NULL;
+
+--changeset you:3
+CREATE TABLE orders (
+  order_id     INT            IDENTITY(1,1) PRIMARY KEY,
+  user_id      INT            NOT NULL
+                       FOREIGN KEY REFERENCES users(id),
+  total_amount DECIMAL(12,2)  NOT NULL,
+  ordered_at   DATETIME       DEFAULT (GETDATE()) NOT NULL
+);
+
+--rollback DROP TABLE orders;
